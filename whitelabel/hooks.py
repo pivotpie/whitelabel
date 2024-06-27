a8cec74a5b42b6ac2e4daf84756ac35a011f5c6e
pivotpie <155568679+pivotpie@users.noreply.github.com>
2024-06-27 11:55:50 +0400

    Update hooks.py

--- whitelabel/hooks.py
+++ whitelabel/hooks.py
@@ -4,15 +4,15 @@ from . import __version__ as app_version
 from . import __logo__ as app_logo
 
 
-app_name = "whitelabel"
-app_title = "Whitelabel"
-app_publisher = "Bhavesh Maheshwari"
-app_description = "ERPNext Whitelabel"
+app_name = "PieERP Whitelabel"
+app_title = "PieERP Whitelabel"
+app_publisher = "Ajay Krishnan"
+app_description = "PieERP Whitelabel"
 app_icon = "octicon octicon-file-directory"
 app_color = "grey"
-app_email = "maheshwaribhavesh95863@gmail.com"
+app_email = "ajayk@pivotpie.com"
 app_license = "MIT"
-app_logo_url = '/assets/whitelabel/images/whitelabel_logo.jpg'
+app_logo_url = '/assets/whitelabel/images/Pie-App.png'
 
 # Includes in <head>
 # ------------------
@@ -49,8 +49,8 @@ web_include_css = "/assets/whitelabel/css/whitelabel_web.css"
 # get_website_user_home_page = "whitelabel.utils.get_home_page"
 
 website_context = {
-	"favicon": app_logo or "/assets/whitelabel/images/whitelabel_logo.jpg",
-	"splash_image": app_logo or "/assets/whitelabel/images/whitelabel_logo.jpg"
+	"favicon": app_logo or "/assets/whitelabel/images/Pie-App.png",
+	"splash_image": app_logo or "/assets/whitelabel/images/Pie-App.png"
 }
 after_migrate = ['whitelabel.api.whitelabel_patch']
 
